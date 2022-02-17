052836804ee185b76a6226d81521867a89095105
Jake Krueger <jmk4u14@gmail.com>
2022-02-16 22:54:08 -0500

    Added render page function for all pages

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Home from "./Home/index";
+import Cinematography from "./Pages/Cinematography";
+import Sales from "./Pages/Sales";
+import WebDesign from "./Pages/WebDesign";
 
 class Portfolio extends Component {
   state = {
@@ -12,6 +15,14 @@ class Portfolio extends Component {
 
   renderPage = () => {
     if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Cinematography") {
+      return <Cinematography />;
+    } else if (this.state.currentPage === "Sales") {
+      return <Sales />;
+    } else if (this.state.currentPage === "WebDesign") {
+      return <WebDesign />;
+    } else {
       return <Home />;
     }
   };
