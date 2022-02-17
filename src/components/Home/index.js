6fe2ff96013f05272cb75c44504692290d6c922e
Jake Krueger <jmk4u14@gmail.com>
2022-02-17 00:48:23 -0500

    Modified homepage with CSS

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,24 @@
 // import { Container, Row, Col } from 'reactstrap';
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 function Home() {
     return (
-        <p>What would you like to know about?</p>
+        <div class="home">
+            <Navbar className="NavDropDown">
+                <Container>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown class="dropdown" title="What would you like to see?">
+                            <NavDropdown.Item className="dropdown-link" href="#">Cinematography</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-link" href="#">Sales</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-link" href="#">Web Design</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     )
 }
 
